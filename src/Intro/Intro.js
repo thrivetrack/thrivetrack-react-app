6a0cc46cd7f0de00c49cc69452971cecca2b0f56
iayasin771@gmail.com <iayasin771@gmail.com>
2023-07-09 16:31:29 -0400

    Stack navigator config setup

--- src/Intro/Intro.js
+++ src/Intro/Intro.js
@@ -8,85 +8,85 @@ import Intro2 from "./Intro2";
 // This is simply a stack you can push and pop from. You will always be on the most recently pushed stack. LIFO ordering
 
 const ProfileScreen = ({ navigation, route }) => {
-	return (
-		<View style={styles.test}>
-			<Text>This is {route.params.a}'s profile</Text>
-			<Button
-				title="Go back!"
-				onPress={() => {
-					navigation.push("Intro1");
-				}}
-			/>
-		</View>
-	);
+  return (
+    <View style={styles.test}>
+      <Text>This is {route.params.a}'s profile</Text>
+      <Button
+        title="Go back!"
+        onPress={() => {
+          navigation.pop();
+        }}
+      />
+    </View>
+  );
 };
 
 const stackNavigItems = [
-	{
-		id: 0,
-		name: "Intro1",
-		component: Intro1,
-		screenComponent: (props) => {
-			return <Intro1 />;
-		},
-		headerShown: false,
-	},
-	{
-		id: 1,
-		name: "Intro2",
-		component: ProfileScreen,
-		screenComponent: (props) => {
-			return <ProfileScreen />;
-		},
-		headerShow: true,
-	},
+  {
+    id: 0,
+    name: "Intro1",
+    component: Intro1,
+    screenComponent: (props) => {
+      return <Intro1 />;
+    },
+    headerShown: false,
+  },
+  {
+    id: 1,
+    name: "Intro2",
+    component: ProfileScreen,
+    screenComponent: (props) => {
+      return <ProfileScreen />;
+    },
+    headerShow: true,
+  },
 ];
 
 const Stack = createNativeStackNavigator();
 
 const Intro = () => {
-	// const pages = [ProfileScreen];
+  // const pages = [ProfileScreen];
 
-	return (
-		<View style={styles.container}>
-			<NavigationContainer>
-				<Stack.Navigator initialRouteName="Home">
-					{stackNavigItems.map((item, index) => {
-						return (
-							<Stack.Screen
-								key={index}
-								name={item.name}
-								component={item.component}
-								options={{
-									title: item.title,
-									headerShown: false,
-								}}
-							/>
-						);
-					})}
-				</Stack.Navigator>
-			</NavigationContainer>
-		</View>
-	);
+  return (
+    <View style={styles.container}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          {stackNavigItems.map((item, index) => {
+            return (
+              <Stack.Screen
+                key={index}
+                name={item.name}
+                component={item.component}
+                options={{
+                  title: item.title,
+                  headerShown: false,
+                }}
+              />
+            );
+          })}
+        </Stack.Navigator>
+      </NavigationContainer>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-	container: {
-		// flex: 1,
-		backgroundColor: "white",
-		// justifyContent: "center",
-		width: "100%",
-		height: "100%",
-	},
-	test: {
-		display: "flex",
-		width: "100%",
-		height: "100%",
-		flexDirection: "column",
-		justifyContent: "center",
-		alignItems: "center",
-		backgroundColor: "white",
-	},
+  container: {
+    // flex: 1,
+    backgroundColor: "white",
+    // justifyContent: "center",
+    width: "100%",
+    height: "100%",
+  },
+  test: {
+    display: "flex",
+    width: "100%",
+    height: "100%",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+  },
 });
 
 export default Intro;
--- src/Intro/Intro1.js
+++ src/Intro/Intro1.js
@@ -1,88 +1,85 @@
 import React, { useState } from "react";
 import {
-	StyleSheet,
-	Text,
-	View,
-	Pressable,
-	TouchableOpacity,
+  StyleSheet,
+  Text,
+  View,
+  Pressable,
+  TouchableOpacity,
 } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import GradientText from "../Utility/GradientText";
 
 export default function Intro1({ navigation, nextScreenName }) {
-	return (
-		<View style={styles.container}>
-			<LinearGradient
-				colors={["#9DCEFF", "#8a9dff"]}
-				start={{ x: 0, y: 0.5 }}
-				end={{ x: 1, y: 0.5 }}
-				style={styles.container}
-			>
-				<Text style={styles.title}>
-					ThriveTrack<Text style={styles.X}>X</Text>
-				</Text>
-				<Text style={styles.smallText}>Everbyody Can Train</Text>
+  return (
+    <View style={styles.container}>
+      <LinearGradient
+        colors={["#9DCEFF", "#8a9dff"]}
+        start={{ x: 0, y: 0.5 }}
+        end={{ x: 1, y: 0.5 }}
+        style={styles.container}
+      >
+        <Text style={styles.title}>
+          ThriveTrack<Text style={styles.X}>X</Text>
+        </Text>
+        <Text style={styles.smallText}>Everbyody Can Train</Text>
 
-				<TouchableOpacity
-					style={styles.button}
-					onPress={() => {
-						// navigation.push("Profile", { a: "aaa", b: "bbb" });
-						navigation.push("Intro2", { a: "aaa", b: "bbb" });
-					}}
-				>
-					<GradientText
-						colors={["blue", "white"]}
-						style={styles.smallText}
-					>
-						Get Started
-					</GradientText>
-				</TouchableOpacity>
-			</LinearGradient>
-		</View>
-	);
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {
+            // navigation.push("Profile", { a: "aaa", b: "bbb" });
+            navigation.push("Intro2", { a: "aaa", b: "bbb" });
+          }}
+        >
+          <GradientText colors={["blue", "white"]} style={styles.smallText}>
+            Get Started
+          </GradientText>
+        </TouchableOpacity>
+      </LinearGradient>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		display: "flex",
-		width: "100%",
-		height: "100%",
-		// padding: "371px 30px 40px 30px",
-		flexDirection: "column",
-		justifyContent: "center",
-		alignItems: "center",
-		// flexShrink: "0",
-		// gap: "271px",
-		// borderRadius: "40px",
-	},
-	title: {
-		color: "#1D1617",
-		fontSize: 36,
-		// fontFamily: "Poppins",
-		fontWeight: "700",
-		// lineHeight: "normal",
-	},
-	X: {
-		color: "white",
-		fontSize: 40,
-		fontWeight: "900",
-	},
-	smallText: {
-		color: "#7B6F72",
-		fontSize: 18,
-		// fontFamily: "Poppins",
-		fontWeight: "400",
-		// lineHeight: "normal",
-	},
-	button: {
-		marginTop: 250,
-		width: 300,
-		alignItems: "center",
-		justifyContent: "center",
-		paddingVertical: 12,
-		paddingHorizontal: 32,
-		borderRadius: 99,
-		elevation: 3,
-		backgroundColor: "white",
-	},
+  container: {
+    display: "flex",
+    width: "100%",
+    height: "100%",
+    // padding: "371px 30px 40px 30px",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    // flexShrink: "0",
+    // gap: "271px",
+    // borderRadius: "40px",
+  },
+  title: {
+    color: "#1D1617",
+    fontSize: 36,
+    // fontFamily: "Poppins",
+    fontWeight: "700",
+    // lineHeight: "normal",
+  },
+  X: {
+    color: "white",
+    fontSize: 40,
+    fontWeight: "900",
+  },
+  smallText: {
+    color: "#7B6F72",
+    fontSize: 18,
+    // fontFamily: "Poppins",
+    fontWeight: "400",
+    // lineHeight: "normal",
+  },
+  button: {
+    marginTop: 250,
+    width: 300,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 99,
+    elevation: 3,
+    backgroundColor: "white",
+  },
 });
